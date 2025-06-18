978c4ac7a297db0d9c398d9a341dbd57087cff2c
Covarel Camille <covarelcamille@gmail.com>
2025-06-18 14:50:09 +0200

    fix: typeerror

--- server/src/modules/item/itemActions.ts
+++ server/src/modules/item/itemActions.ts
@@ -1,13 +1,13 @@
 import type { RequestHandler } from "express";
 
 // Import access to data
-import itemRepository from "./itemRepository";
+import { User } from "../../models/user.model"
 
 // The B of BREAD - Browse (Read All) operation
 const browse: RequestHandler = async (req, res, next) => {
   try {
     // Fetch all items
-    const items = await itemRepository.readAll();
+    const items = await User.findAll();
 
     // Respond with the items in JSON format
     res.json(items);
@@ -22,7 +22,7 @@ const read: RequestHandler = async (req, res, next) => {
   try {
     // Fetch a specific item based on the provided ID
     const itemId = Number(req.params.id);
-    const item = await itemRepository.read(itemId);
+    const item = await User.findOne({where: {id: itemId}});
 
     // If the item is not found, respond with HTTP 404 (Not Found)
     // Otherwise, respond with the item in JSON format
@@ -40,14 +40,20 @@ const read: RequestHandler = async (req, res, next) => {
 // The A of BREAD - Add (Create) operation
 const add: RequestHandler = async (req, res, next) => {
   try {
-    // Extract the item data from the request body
-    const newItem = {
-      title: req.body.title,
-      user_id: req.body.user_id,
-    };
 
     // Create the item
-    const insertId = await itemRepository.create(newItem);
+    const insertId = await User.findOrCreate({defaults: {
+        firstName: 'Test',
+        lastName: 'User',
+        email: 'test.user@example.com',
+        password: 'securepassword123',
+        birthdate: new Date('1990-01-01'),
+        address: '123 Main St',
+        city: 'Anytown',
+        postcode: '12345',
+        country: 'FR',
+        isAdmin: false,
+      },});
 
     // Respond with HTTP 201 (Created) and the ID of the newly inserted item
     res.status(201).json({ insertId });
