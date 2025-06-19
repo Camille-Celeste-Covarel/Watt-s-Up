3ee678bf049c87ff58235cc25c2abc338805261d
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-06-19 11:33:01 +0200

    feat: finalise topbar

--- client/src/components/topbar/TopBar.tsx
+++ client/src/components/topbar/TopBar.tsx
@@ -1,19 +1,11 @@
 import "./topbar.css";
 
-import filtre from "../../assets/images/topbar/filtre.svg";
 import logo from "../../assets/images/topbar/logo.svg";
-import loupe from "../../assets/images/topbar/loupe.svg";
 
 function TopBar() {
   return (
     <div className="topbar-container">
       <img src={logo} alt="logo" className="logo" />
-      <div className="topbar-search">
-        <div className="searchbar">
-          <img src={loupe} alt="" className="loupe" />
-        </div>
-        <img src={filtre} alt="filtre" className="filtre" />
-      </div>
     </div>
   );
 }
