64dab192cd5f9c05a86d262ec3f567131e1c9526
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-06-20 09:59:24 +0200

    feat: initialize react router and create landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,14 @@
 import "./normalize.css";
 import "./App.css";
-import MapLibre from "./components/map/MapLibre";
+import { Outlet } from "react-router";
 import NavBar from "./components/navbar/NavBar";
-import SearchBar from "./components/searchbar/SearchBar";
 import TopBar from "./components/topbar/TopBar";
 
 function App() {
   return (
     <>
       <TopBar />
-      <SearchBar />
-      <MapLibre />
+      <Outlet />
       <NavBar />
     </>
   );
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import MapLibre from "../components/map/MapLibre";
+import SearchBar from "../components/searchbar/SearchBar";
+
+function LandingPage() {
+  return (
+    <>
+      <SearchBar />
+      <MapLibre />
+    </>
+  );
+}
+
+export default LandingPage;
