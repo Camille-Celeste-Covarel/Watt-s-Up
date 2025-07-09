dd019e80c6bf0bf97940a23d7f3782a62095c568
Covarel <covarelcamille@gmail.com>
2025-07-10 00:31:03 +0200

    fix doc and delete old tests

--- server/tests/install.test.ts
+++ server/tests/install.test.ts
@@ -1,16 +1,7 @@
 // Load environment variables from .env file
 import "dotenv/config";
 
-import fs from "node:fs";
-
-import databaseClient from "../database/client";
-
-import type { Rows } from "../database/client";
-
-// Close the database connection after all tests have run
-afterAll((done) => {
-  databaseClient.end().then(done);
-});
+import * as fs from "node:fs";
 
 // Test suite for environment installation
 describe("Installation", () => {
@@ -19,29 +10,8 @@ describe("Installation", () => {
     expect(fs.existsSync(`${__dirname}/../.env`)).toBe(true);
   });
 
-  // Test: Check if the .env.sample file exists
-  test("You have retained /server/.env.sample", async () => {
+  // Test: Check if the .env.development.sample file exists
+  test("You have retained /server/.env.development.sample", async () => {
     expect(fs.existsSync(`${__dirname}/../.env.sample`)).toBe(true);
-  });
-
-  // Test: Check if the .env file is properly filled with valid database connection information
-  test("You have filled /server/.env with valid information to connect to your database", async () => {
-    expect.assertions(0);
-
-    try {
-      // Check if the connection is successful
-      await databaseClient.getConnection();
-    } catch (error) {
-      expect(error).toBeDefined();
-    }
-  });
-
-  // Test: Check if the database migration scripts have been executed
-  test("You have executed the db:migrate scripts", async () => {
-    // Query the 'item' table to check if any data has been inserted
-    const [rows] = await databaseClient.query<Rows>("select * from item");
-
-    // Expecting rows to be returned, indicating successful migration
-    expect(rows.length).toBeGreaterThanOrEqual(0);
   });
 });
