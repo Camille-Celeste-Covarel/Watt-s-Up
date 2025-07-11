ca51cc667d72f1ebfb7b258db2d407734a2d2f72
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-07-11 16:21:31 +0200

    feat: start RegisterPage

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,5 +1,15 @@
 function RegisterPage() {
-  return <div>test</div>;
+  return (
+    <div className="register-container">
+      <section className="register-profil-section">
+        <h2>Mon profil</h2>
+        <div className="profil-picture-container">
+          <button type="button">Télécharger une photo</button>
+        </div>
+        <form>test</form>
+      </section>
+    </div>
+  );
 }
 
 export default RegisterPage;
