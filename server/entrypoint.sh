bed706229da8f861ed6edb53a5e27074f81b80d4
Covarel <covarelcamille@gmail.com>
2025-10-25 00:47:49 +0200

    new backend pipeline CI/CD, add seeder first admin account

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,13 +5,16 @@ set -e
 
 # Affiche un message pour indiquer que les migrations commencent
 echo "Running database migrations..."
-
-# Exécute la commande de migration de Sequelize
-# 'npx' est utilisé pour exécuter le binaire de sequelize-cli installé dans node_modules
 npx sequelize-cli db:migrate
+echo "Migrations finished."
+
+# Affiche un message pour indiquer que les seeders commencent
+echo "Running database seeders..."
+npx sequelize-cli db:seed:all
+echo "Seeders finished."
 
 # Affiche un message avant de lancer l'application
-echo "Migrations finished. Starting the application..."
+echo "Starting the application..."
 
 # 'exec "$@"' exécute la commande passée en argument au script.
 # Dans notre cas, ce sera la commande pour démarrer le serveur (ex: "npm start").
