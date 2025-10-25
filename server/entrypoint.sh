1dcea7f577aed6f48e727024c854258b477fd969
Covarel <covarelcamille@gmail.com>
2025-10-25 02:17:51 +0200

    fix path script backend

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,17 +5,18 @@ set -e
 
 # Affiche un message pour indiquer que les migrations commencent
 echo "Running database migrations..."
-npx sequelize-cli db:migrate
+# Utilise le script npm du workspace pour s'assurer du bon contexte
+npm run migrate:prod --workspace=server
 echo "Migrations finished."
 
 # Affiche un message pour indiquer que les seeders commencent
 echo "Running database seeders..."
-npx sequelize-cli db:seed:all
+# Utilise le script npm du workspace pour s'assurer du bon contexte
+npm run db:seed:prod --workspace=server
 echo "Seeders finished."
 
 # Affiche un message avant de lancer l'application
 echo "Starting the application..."
 
-# 'exec "$@"' exécute la commande passée en argument au script.
-# Dans notre cas, ce sera la commande pour démarrer le serveur (ex: "npm start").
+# 'exec "$@"' exécute la commande passée en argument au script (CMD du Dockerfile).
 exec "$@"
