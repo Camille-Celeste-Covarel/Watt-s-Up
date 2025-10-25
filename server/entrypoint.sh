48809ed06d31514c30c6f1025a4f3cf1759ec36f
Covarel <covarelcamille@gmail.com>
2025-10-25 03:12:29 +0200

    fix

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+cd /app/server
+
 echo "Running database migrations..."
-npm run migrate:prod --workspace=server
+npm run migrate:prod
 echo "Migrations finished."
 
 echo "Running database seeders..."
-npm run db:seed:prod --workspace=server
+npm run db:seed:prod
 echo "Seeders finished."
 
 echo "Starting the application..."
