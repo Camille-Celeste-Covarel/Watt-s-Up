004147a3c56653972d79fc0396b638ade74cb9a6
Covarel <covarelcamille@gmail.com>
2025-10-25 13:48:03 +0200

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -47,11 +47,5 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 };
